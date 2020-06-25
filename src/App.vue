c5693063c1956989c3f52a178967288280586664
Roland Doda <rolanddoda2014@gmail.com>
2020-06-25 09:11:27 +0200

    Change to trigger refresh

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      A RANDOM CHANGE
+      <hr />
+      Change to trigger refresh
     </div>
     <router-view />
   </div>
